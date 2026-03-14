e21ff0c31747022215ee2582cd5e7d90215dbb21
Eyapènè Bokobosso <33751533+eyapene@users.noreply.github.com>
2026-03-14 06:12:52 +0100

    Update utilities.py

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -1,1 +1,33 @@
+def show_samples(label):
 
+    folder = os.path.join(train_dir, label)
+    files = os.listdir(folder)[:5]
+
+    plt.figure(figsize=(12,3))
+
+    for i, file in enumerate(files):
+
+        img = Image.open(os.path.join(folder, file))
+
+        plt.subplot(1,5,i+1)
+        plt.imshow(img, cmap="gray")
+        plt.axis("off")
+
+    plt.suptitle(label)
+    plt.show()
+
+
+def compute_brightness(folder, sample_size=300):
+
+    brightness = []
+
+    files = os.listdir(folder)[:sample_size]
+
+    for file in files:
+
+        img = Image.open(os.path.join(folder, file)).convert("L")
+        img_array = np.array(img)
+
+        brightness.append(img_array.mean())
+
+    return brightness
